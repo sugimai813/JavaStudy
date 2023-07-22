8a4dae963fbcb09ac39563e7a89286c08959c57a
sugimai813 <maaaimaaai813@gmail.com>
2023-07-22 17:18:15 +0900

    課題①

--- springSample1-1/src/main/java/com/example/demo/controller/UserController.java
+++ springSample1-1/src/main/java/com/example/demo/controller/UserController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.demo.entity.UserEntity;
@@ -32,8 +33,25 @@ public class UserController {
 	  List<UserEntity> userlist = userService.searchAll();
 	  model.addAttribute("userlist", userlist);
 	  return "user/list";
+      }
 	
-
-
-  }
+	  /**
+		 * ユーザー新規登録画面を表示
+		 * @param  model Model
+		 * @return  ユーザー情報一覧画面
+		 */
+	@RequestMapping("/user/add")
+	  public String displayAdd(Model model) {
+	  return "user/add";
+	 }
+		/**
+		 * ユーザー情報詳細画面を表示
+		 * @param  id 表示するユーザーID
+		 * @param  model Model
+		 * @return  ユーザー情報詳細画面
+		 */
+	@RequestMapping("/user/{id}")
+	 public String displayView(@PathVariable  Integer id, Model model) {
+	 return "user/view";
+	 }
 }
