e2fe141594668db9e61aada9fdf29fba665cb55d
sugimai813 <maaaimaaai813@gmail.com>
2023-07-27 00:12:10 +0900

    課題③

--- springSample1-1/src/main/java/com/example/demo/controller/UserController.java
+++ springSample1-1/src/main/java/com/example/demo/controller/UserController.java
@@ -1,14 +1,22 @@
 package com.example.demo.controller;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.demo.dto.UserRequest;
 import com.example.demo.entity.UserEntity;
 import com.example.demo.service.UserService;
+
 /**
  * ユーザー情報 Controller
  */
@@ -20,6 +28,9 @@ public class UserController {
   //2行追加
   @Autowired
   UserService userService;
+  
+//  @Autowired
+//  UserRequest userRequest;
 
   /**
    * ユーザー情報一覧画面を表示
@@ -34,23 +45,53 @@ public class UserController {
 	  model.addAttribute("userlist", userlist);
 	  return "user/list";
       }
+  
+  /**
+	 * ユーザー新規登録画面を表示
+	 * @param  model Model
+	 * @return  ユーザー情報一覧画面
+	 */
+@RequestMapping("/user/add")
+  public String displayAdd(Model model) {
+	 model.addAttribute("userRequest", new UserRequest());
+     return "user/add";
+ }
+
+  /**
+	 * ユーザー新規登録
+	 * @param  userRequest リクエストデータ
+	 * @param  model Model
+	 * @return  ユーザー情報一覧画面
+	 */
+	@RequestMapping("/user/create")
+	public String create(@Validated @ModelAttribute UserRequest userRequest, BindingResult result, Model model) {
+		//入力判定入れること
+		if(result.hasErrors()) {
+			// 入力チェックエラーの場合3行実装
+			List<String> errorList = new ArrayList<String>();
+			for(ObjectError error : result.getAllErrors()) {
+				errorList.add(error.getDefaultMessage());
+			}
+		
+					
+
+			//エラー判定後の画面遷移2行実装
+			model.addAttribute("validationError", errorList);
+			return "user/add";
+
+		}
+		// ユーザー情報の登録2行実装
+		userService.create(userRequest);
+		return "redirect:/user/list";
+	}
 	
-	  /**
-		 * ユーザー新規登録画面を表示
-		 * @param  model Model
-		 * @return  ユーザー情報一覧画面
-		 */
-	@RequestMapping("/user/add")
-	  public String displayAdd(Model model) {
-	  return "user/add";
-	 }
 		/**
 		 * ユーザー情報詳細画面を表示
 		 * @param  id 表示するユーザーID
 		 * @param  model Model
 		 * @return  ユーザー情報詳細画面
 		 */
-	@RequestMapping("/user/{id}")
+	@GetMapping("/user/{id}")
 	 public String displayView(@PathVariable  Integer id, Model model) {
 	 return "user/view";
 	 }
--- springSample1-1/src/main/java/com/example/demo/service/UserService.java
+++ springSample1-1/src/main/java/com/example/demo/service/UserService.java
@@ -1,11 +1,14 @@
 package com.example.demo.service;
 
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.example.demo.dto.UserRequest;
 import com.example.demo.entity.UserEntity;
 import com.example.demo.repository.UserRepository;
 
@@ -20,7 +23,7 @@ public class UserService {
 	 */
 	//2行追加
    @Autowired
-   private UserRepository userRepository;
+   private static UserRepository userRepository;
 
 
 	/**
@@ -32,4 +35,30 @@ public class UserService {
 		return userRepository.findAll();
 
 	}
+	
+	/**
+	 * ユーザー情報 主キー検索
+	 * @return  検索結果
+	 */
+	public Optional<UserEntity> findById(Integer id) {
+		//実装1行
+		return userRepository.findById(id);
+	}
+
+	/**
+	 * ユーザー情報 新規登録
+	 * @param  user ユーザー情報
+	 */
+	public static void create(UserRequest userRequest) {
+		Date now = new Date();
+		UserEntity user = new UserEntity();
+                        //実装2行
+		user.setName(userRequest.getName());
+		user.setAddress(userRequest.getAddress());
+		user.setPhone(userRequest.getPhone());
+		user.setCreateDate(now);
+		user.setUpdateDate(now);
+                       //保存するメソッド実装1行
+		userRepository.save(user);
+}
 }
